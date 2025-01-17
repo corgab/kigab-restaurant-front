fc291d9b26b2279ef9fff81c5ae6e7501b0eb00e
corgab <corbanigabriele@gmail.com>
2025-01-17 16:18:38 +0100

    fix responsive

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,8 +2,8 @@
     <div>
         <div :style="`background-image: url(${section.image_url});`" class="bg-cover bg-center ">
             <div class="container mx-auto lg:w-2/6 py-40 text-center text-main">
-                <h1 class="play-fair leading-tight my-7 text-8xl">{{ section.title }}</h1>
-                <p class="leading-base my-7 font-sans text-2xl">{{ section.content }}</p>
+                <h1 class="play-fair leading-tight my-7 text-5xl sm:text-8xl">{{ section.title }}</h1>
+                <p class="leading-base my-7 font-sans text-xl sm:text-2xl">{{ section.content }}</p>
                 <div class="my-11 flex justify-around">
                     <Button text="Book a table" :textColor="'text-main'" :isRed="true" />
                     <Button text="Book a table" :textColor="'text-main'" :isRed="false" />
